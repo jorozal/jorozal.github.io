7b3f04676188ec84ac6edb1f202384dd4446dc5a
Jerusalem <149168327+jorozal@users.noreply.github.com>
2023-12-07 00:42:52 +0100

    hert.php

--- hert.php
+++ hert.php
@@ -1,1 +1,15 @@
 
+<!DOCTYPE html>
+<html>
+<head>
+  <title>PHP Show Text in Div</title>
+</head>
+<body>
+  <div>
+    <?php
+      $text = "Hello, world!"; // Text to display
+      echo $text;
+    ?>
+  </div>
+</body>
+</html>
